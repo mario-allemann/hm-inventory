cc5aaa0b3aa76c06f6bf59306add69dcfa046ac1
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 14:26:07 +0100

    removed stuff

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into student (name, passport_number)
-values('Ranga', 'E1234567');
-
-insert into student (name, passport_number)
-values('Ravi', 'A1234568');
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists student;
-create table student
-(
-   id integer not null auto_increment,
-   name varchar(255) not null,
-   passport_number varchar(255) not null,
-   primary key(id)
-);
