90b5dc328a967e5a313b898cca5b8bb9689726c3
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 14:16:16 +0100

    ok cool

--- src/main/java/fhnw/hminventory/HmInventoryApplication.java
+++ src/main/java/fhnw/hminventory/HmInventoryApplication.java
@@ -1,5 +1,6 @@
 package fhnw.hminventory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,7 @@ import fhnw.hminventory.domain.WarehouseRepo;
 @Controller
 @EnableAutoConfiguration
 public class HmInventoryApplication {
-
-	
+	@Autowired
 	private WarehouseRepo whRepo;
 	
 	public static void main(String[] args) {
