9fd6c6c8f6faded41ca215c18b6c8d5060135b12
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 14:33:37 +0100

    ok cool

--- src/main/java/fhnw/hminventory/domain/WarehouseRepo.java
+++ src/main/java/fhnw/hminventory/domain/WarehouseRepo.java
@@ -1,10 +1,8 @@
 package fhnw.hminventory.domain;
-
 import org.springframework.data.repository.CrudRepository;
 
 //This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 //CRUD refers Create, Read, Update, Delete
-
 public interface WarehouseRepo extends CrudRepository<Warehouse, Integer> {
 
 }
