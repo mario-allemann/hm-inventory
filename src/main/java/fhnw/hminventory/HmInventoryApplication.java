e69d83d08f0a618d3fafbf82524b936538a5c4b0
Mario <Mario@MARIO-PC>
2018-12-16 13:06:27 +0100

    fresh init

---
+++ src/main/java/fhnw/hminventory/HmInventoryApplication.java
@@ -0,0 +1,14 @@
+package fhnw.hminventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmInventoryApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/fhnw/hminventory/HmInventoryApplicationTests.java
@@ -0,0 +1,17 @@
+package fhnw.hminventory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HmInventoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
